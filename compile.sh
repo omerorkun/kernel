72dacbe99a26a83de7f9ceaca8fd7ccb2f206a54
Omer Orkun Duztas <omerorkun@buyutech.com.tr>
2019-05-02 17:47:06 +0300

    cptoboot.sh added

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@ export CROSS_COMPILE=arm-linux-gnueabihf-
 export ARCH=arm
 
 make buyutech_rk3288_defconfig
-make -j8
+make -j16
---
+++ cptoboot.sh
@@ -0,0 +1,2 @@
+cp arch/arm/boot/zImage ~/boot/
+cp arch/arm/boot/dts/rk3288-miniarm.dtb ~/boot/
